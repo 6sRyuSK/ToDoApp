6587f8c390a11780584a220af78edf8f3c515969
6sRyuSK <zikkyou19@gmail.com>
2020-06-01 19:08:39 +0900

    feat: create ToDoList

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,65 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>HELLO</h1>
+    <ul class="to-do-list">
+      <li class="to-do-item" v-for="item in toDoList" :key="item.createdAt.getTime()">
+        <h1>
+          {{ item.title }}
+        </h1>
+        <h2>{{ item.description }}</h2>
+        <h3>{{ item.priority }}</h3>
+        <h3>{{ item.createdAt }}</h3>
+        <h3>残り日数{{ item.getFormattedTime() }}</h3>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      toDoList: [
+        {
+          title: 'first TODO',
+          description: 'first time',
+          createdAt: new Date('2020-06-01 17:42'),
+          deadLine: new Date('2020-06-11 17:42'),
+          priority: 'high',
+          getLastDay () {
+            const lastDay = this.deadLine.getTime() - (new Date().getTime() + 9 * 60 * 60 * 1000)
+            return new Date(lastDay)
+          },
+          getFormattedTime () {
+            const time = this.getLastDay().toLocaleTimeString()
+            const date = this.getLastDay().getDate()
+            const month = this.getLastDay().getMonth()
+            return `${month} / ${date} / ${time}`
+          }
+        },
+        {
+          title: '2nd TODO',
+          description: '2nd time',
+          createdAt: new Date('2020-06-02 17:42'),
+          deadLine: new Date('2020-06-01 19:10:00'),
+          priority: 'low',
+          getLastDay () {
+            const lastDay = this.deadLine.getTime() - (new Date().getTime() + 9 * 60 * 60 * 1000)
+            return new Date(lastDay)
+          },
+          getFormattedTime () {
+            const time = this.getLastDay().toLocaleTimeString()
+            const date = this.getLastDay().getDate()
+            const month = this.getLastDay().getMonth()
+            return `${month} / ${date} / ${time}`
+          }
+        }
+      ]
+    }
+  },
+  created () {
+    console.log(this.toDoList[0].createdAt)
   }
 }
 </script>
